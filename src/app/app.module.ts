89786b12e130c961a3e57a6d2faa49378ee73c0a
Alexandradra <alexandra.dubost@gmail.com>
2020-04-08 14:33:15 +0200

    TP V2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+// Service contenant le JSON de tous les musées et les méthodes associées
+import { MuseumListService } from './services/museumList.service';
+// Liste des musée : inclue SingleMuseumComponent
+import { MuseumListComponent } from './museum-list/museum-list.component';
+// Un musée dans la liste
 import { SingleMuseumComponent } from './single-museum/single-museum.component';
 
-import { MuseumListService } from './services/museumList.service';
-import { MuseumListComponent } from './museum-list/museum-list.component';
-import { AddComponent } from './add/add.component';
-import { MuseumDetailComponent } from './museum-detail/museum-detail.component';
-
-
 const appRoutes:Routes = [
-  {path: 'add', component: AddComponent},
-  {path: 'singleMuseum', component: SingleMuseumComponent},
-  {path: '', component: MuseumListComponent},
-  {path: 'museum/:museumId', component: MuseumDetailComponent}
+  {path: '', component: MuseumListComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     SingleMuseumComponent,
-    AddComponent,
     MuseumListComponent,
-    MuseumDetailComponent
   ],
   imports: [
     BrowserModule,
